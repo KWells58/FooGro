53809310c7db943a45bac97d1a2dca9c5d0e9553
KWells58 <kelly.wells10000@gmail.com>
2023-11-16 19:39:33 -0500

    changed homescreen, removed grocery screen

--- app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/foogro_1/MainActivity.java
+++ app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/foogro_1/MainActivity.java
@@ -2,9 +2,12 @@ package edu.floridapoly.mobiledeviceapps.fall23.foogro_1;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,38 +21,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         // Initialize DatabaseHelper
         dbHelper = new DatabaseHelper(this);
-
         JSONDataImporter importer = new JSONDataImporter(this);
-
         // Import data from Google Drive
         importer.importFromGoogleDrive();
 
-        // Check if first launch and add a sample product to the database
-        addSampleProductToDatabase();
+        EditText searchText = findViewById(R.id.editTextText);
+        Button searchButton = findViewById(R.id.search_button);
 
-        Button profileHomeButton = findViewById(R.id.profile_home_button);
-        Button groceryButton = findViewById(R.id.grocery_button);
-
-        profileHomeButton.setOnClickListener(new View.OnClickListener() {
+        searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showToast("Profile/Home Button Clicked");
-                // Start the Profile Screen
-                Intent intent = new Intent(MainActivity.this, Profile_Screen.class);
-                startActivity(intent);
-            }
-        });
-
-        groceryButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showToast("Grocery Button Clicked");
-                // Start the Grocery Screen
-                Intent intent = new Intent(MainActivity.this, Grocery_Screen.class);
-                startActivity(intent);
+                String query = searchText.getText().toString();
+                searchProducts(query);
             }
         });
     }
@@ -62,4 +47,31 @@ public class MainActivity extends AppCompatActivity {
     private void showToast(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
+
+    private void searchProducts(String query) {
+        TextView resultsView = findViewById(R.id.tvResults);
+        Cursor cursor = dbHelper.searchProducts(query);
+        StringBuilder results = new StringBuilder();
+
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                String productName = cursor.getString(cursor.getColumnIndexOrThrow("name"));
+                String category = cursor.getString(cursor.getColumnIndexOrThrow("category"));
+                double price = cursor.getDouble(cursor.getColumnIndexOrThrow("price"));
+                String storeName = cursor.getString(cursor.getColumnIndexOrThrow("storeName"));
+                int storeID = cursor.getInt(cursor.getColumnIndexOrThrow("storeID"));
+
+                results.append("Name: ").append(productName)
+                        .append(", Category: ").append(category)
+                        .append(", Price: ").append(price)
+                        .append(", Store Name: ").append(storeName)
+                        .append(", Store ID: ").append(storeID)
+                        .append("\n\n");
+            }
+            cursor.close();
+        }
+
+        resultsView.setText(results.toString());
+    }
+
 }
