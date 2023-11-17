f6e90f4ad6a249259c9cab0388d84bce46aa884c
RButcher0131 <rupertbutcher4572@gmail.com>
2023-11-16 22:05:18 -0500

    New Classes

--- app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/foogro_1/Ai_QueryScreen.java
+++ app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/foogro_1/Ai_QueryScreen.java
@@ -3,11 +3,11 @@ package edu.floridapoly.mobiledeviceapps.fall23.foogro_1;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Ai_Meals extends AppCompatActivity {
+public class Ai_QueryScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ai_meals);
+        setContentView(R.layout.activity_ai_queryscreen);
     }
 }
---
+++ app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/foogro_1/User_List_Screen.java
@@ -0,0 +1,14 @@
+package edu.floridapoly.mobiledeviceapps.fall23.foogro_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class User_List_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_list_screen);
+    }
+}
